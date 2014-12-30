c4f6b7cd544a3e2bd0b854c2b2ebea8d64f6e6aa
Sergiu Rosca <rmagnum2002@gmail.com>
2014-12-30 16:09:03 +0200

    first commit

---
+++ src/scanner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String args[]){
+		Scanner bucky = new Scanner(System.in);
+		System.out.println(bucky.nextLine());
+		bucky.close();
+	}
+}
