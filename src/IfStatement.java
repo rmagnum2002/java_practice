5a509d88c40a5d51c5d59c33cbd8e897a1c4bfd3
Sergiu Rosca <rmagnum2002@gmail.com>
2014-12-30 18:32:58 +0200

    more lessons

---
+++ src/IfStatement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class IfStatement{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int test = 6;
+		
+		if (test == 9) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+			test = input.nextInt();
+			System.out.println((test == 9 ? "Yes" : "No"));
+			input.close();
+		}
+	}
+}
